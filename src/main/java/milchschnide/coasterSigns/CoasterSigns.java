de1b000a53e945f3e9aa546c789d4efd88ca3ebd
milchschnide <schdt.daniel@gmail.com>
2026-01-31 20:40:56 +0100

    init

---
+++ src/main/java/milchschnide/coasterSigns/CoasterSigns.java
@@ -0,0 +1,17 @@
+package milchschnide.coasterSigns;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CoasterSigns extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
