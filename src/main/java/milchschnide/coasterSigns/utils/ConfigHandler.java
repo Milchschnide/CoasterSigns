3bf72767f183f601c423a10b5b2a82a4a372e005
milchschnide <schdt.daniel@gmail.com>
2026-01-31 20:44:47 +0100

    feat: add ConfigHandler

---
+++ src/main/java/milchschnide/coasterSigns/utils/ConfigHandler.java
@@ -0,0 +1,72 @@
+package milchschnide.coasterSigns.utils;
+
+import lombok.Getter;
+import milchschnide.coasterSigns.CoasterSigns;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import javax.annotation.Nonnull;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+
+public final class ConfigHandler {
+
+    @Getter
+    private static YamlConfiguration configuration;
+
+    /**
+     * Initializes the configuration file.
+     *
+     * @param dataFolder the data folder of the plugin
+     */
+    public static void initConfig(@Nonnull File dataFolder) {
+        final File configPath = new File(dataFolder.getPath(), "settings.yml");
+        if (configPath.exists()) {
+            configuration = YamlConfiguration.loadConfiguration(configPath);
+            return;
+        }
+        configuration = createConfig(configPath);
+        if (configuration != null) return;
+        CoasterSigns.instance.getLogger().log(Level.SEVERE, () -> "[CoasterSigns] Konnte die settings.yml Datei nicht erstellen!");
+        Bukkit.getPluginManager().disablePlugin(CoasterSigns.instance);
+    }
+
+    /**
+     * Creates the configuration file.
+     *
+     * @param configPath the path to the configuration file
+     * @return the created configuration
+     */
+    private static YamlConfiguration createConfig(@Nonnull File configPath) {
+        final YamlConfiguration conf = new YamlConfiguration();
+        conf.addDefault("defaults.friction", 0.3);
+        conf.addDefault("defaults.gravity", 1.5);
+        conf.addDefault("defaults.brakes.speed", 14);
+        conf.addDefault("defaults.brakes.distance", 15);
+        conf.addDefault("defaults.launch.speed", 50);
+        conf.addDefault("defaults.launch.distance", 20);
+        conf.addDefault("defaults.launch.waitTime", 3);
+        conf.options().copyDefaults(true);
+        try {
+            conf.save(configPath);
+        } catch (IOException ex) {
+            return null;
+        }
+        return conf;
+    }
+
+    /**
+     * Loads the default values from the configuration into the CoasterSigns class.
+     */
+    public static void loadDefaults() {
+        CoasterSigns.defaultFriction = configuration.getDouble("defaults.friction");
+        CoasterSigns.defaultGravity = configuration.getDouble("defaults.gravity");
+        CoasterSigns.defaultTravelSpeed = (configuration.getDouble("defaults.brakes.speed") / 3.6) / 20;
+        CoasterSigns.defaultBrakeDistance = configuration.getDouble("defaults.brakes.distance");
+        CoasterSigns.defaultLaunchSpeed = (configuration.getDouble("defaults.launch.speed") / 3.6) / 20;
+        CoasterSigns.defaultLaunchDistance = configuration.getDouble("defaults.launch.distance");
+        CoasterSigns.defaultWaitTime = configuration.getDouble("defaults.launch.waitTime");
+    }
+
+}
