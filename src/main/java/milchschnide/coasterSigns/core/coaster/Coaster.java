c43e0b8664f0213f834a8d5fe6607817ab67f626
milchschnide <schdt.daniel@gmail.com>
2026-02-02 17:44:51 +0100

    build: implement new changes

--- src/main/java/milchschnide/coasterSigns/core/coaster/Coaster.java
+++ src/main/java/milchschnide/coasterSigns/core/coaster/Coaster.java
@@ -1,10 +1,11 @@
 package milchschnide.coasterSigns.core.coaster;
 
 import com.bergerkiller.bukkit.tc.Station.StationConfig;
+import com.bergerkiller.bukkit.tc.controller.MinecartGroup;
 import com.bergerkiller.bukkit.tc.controller.MinecartMember;
 import lombok.Getter;
 import lombok.Setter;
-import milchschnide.coasterSigns.core.Block;
+import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 
 import java.util.ArrayList;
@@ -14,18 +15,28 @@ public class Coaster {
     private final String name;
     private final List<Block> blocks = new ArrayList<>();
     private final StationConfig stationConfig;
-    private final BlockFace direction;
+    @Getter
+    @Setter
+    private BlockFace direction;
     @Getter
     private final CountDownHandler countDownHandler = new CountDownHandler(this);
+    private PassThroughHandler passThroughHandler = null;
     @Setter
     private MinecartMember<?> trainInStation;
     private boolean gatesClosed = false;
+    @Setter
+    private org.bukkit.block.Block block;
     private boolean restraintsClosed = false;
 
     public Coaster(String name, StationConfig stationConfig, BlockFace direction) {
         this.name = name;
         this.stationConfig = stationConfig;
         this.direction = direction;
+    }
+
+    public Coaster init() {
+        CoasterCHACHE.addCoaster(this);
+        return this;
     }
 
     public String name() {
@@ -36,17 +47,39 @@ public class Coaster {
         return trainInStation != null;
     }
 
+    public void openGates() {
+        //open gates logic
+    }
+
     public void closeGates() {
         //close gates logic
     }
 
-    public void closeRestraints() {
+    public void openRestraints(MinecartGroup group) {
+        //open restraints logic
+    }
+
+    public void closeRestraints(MinecartGroup group) {
         //close restraints logic
     }
 
     public void launchTrain() {
         if (trainInStation == null) throw new IllegalStateException("Train has not been set");
-        trainInStation.getActions().addActionLaunch(direction, stationConfig.getLaunchConfig(),stationConfig.getLaunchSpeed());
+        trainInStation.getActions().addActionLaunch(direction, stationConfig.getLaunchConfig(), stationConfig.getLaunchSpeed());
+    }
+
+    public void startCountDown(MinecartGroup group) {
+        countDownHandler.startCountdown(group);
+    }
+
+    public void stopCountDown() {
+        countDownHandler.stopCountdown();
+    }
+
+    public void setPassThroughs(int passThroughs) {
+        if (passThroughHandler == null) {
+            passThroughHandler = new PassThroughHandler(this, passThroughs);
+        }
     }
 
 }
